6ddc62985926406755aac3984ba287284ae9a4bf
MixFix7 <mashtalar95@gmail.com>
2023-10-08 19:25:16 +0300

    adding command to bot

--- BotApi/core/apps/bot/bot_messges.py
+++ BotApi/core/apps/bot/bot_messges.py
@@ -1,3 +1,26 @@
 welcome_message = """
     Hi there, let's start development a new telegram bot? 
 """
+
+error_message = "Error, please try again later"
+
+please_write_name_for_chat = """
+    <b>Write a name for the new chat</b>
+"""
+
+chat_created_successfully = """
+    <b>The chat has been created, now send a description of the person or creature that the chatbot will imitate.</b>
+"""
+
+you_talking_with = """
+    <b>You are talking with chatbot: </b>
+"""
+
+to_exit = """
+    <b>To exit, write the command: /exit </b>
+"""
+
+you_out = """
+    You are out.
+"""
+
--- BotApi/core/apps/bot/main_bot.py
+++ BotApi/core/apps/bot/main_bot.py
@@ -1,12 +1,16 @@
+import telebot
 from telebot.async_telebot import AsyncTeleBot
+from telebot import State
 from django.conf import settings
 from .bot_messges import *
 from . import logic
 
+from core.apps.chats.models import *
+
+
 bot = AsyncTeleBot(settings.BOT_TOKEN, parse_mode='HTML')
 
-
-print('Bot started successfully!!!')
+state = {}
 
 
 async def main():
@@ -20,8 +24,44 @@ async def send_welcome(message):
     await bot.reply_to(message, welcome_message)
 
 
-@bot.message_handler(func=lambda message: True)
-async def echo_all(message):
+@bot.message_handler(commands=['speaking_with_bot'])
+async def handle_speaking_with_chatbot(message):
+    await bot.send_message(message.chat.id, you_talking_with + '<strong>Chatbot</strong>')
+    await bot.send_message(message.chat.id, to_exit)
+    state[message.from_user.id] = 'speaking_with_bot'
+
+
+@bot.message_handler(func=lambda message: state.get(message.from_user.id) == 'speaking_with_bot')
+async def chatbot_message(message):
     gpt_answer = logic.generate_response(message.text)
     await bot.send_message(message.chat.id, gpt_answer)
+
+
+@bot.message_handler(commands=['new_chat'])
+async def handle_new_chat(message):
+    await bot.send_message(message.chat.id, please_write_name_for_chat)
+    state[message.from_user.id] = 'new_chat'
+
+
+@bot.message_handler(func=lambda message: state.get(message.from_user.id) == 'new_chat')
+async def start_new_chat(message):
+    new_chat_data = {
+        'chat_id': message.chat.id,
+        'name': message.text,
+        'chat_username': message.sender.id,
+    }
+
+    try:
+        Chat.objects.create(new_chat_data)
+        await bot.send_message(message.chat.id, chat_created_successfully)
+    except Exception as e:
+        await bot.send_message(message.chat.id, error_message)
+
+
+@bot.message_handler(commands=['quit'])
+async def quit(message):
+    await bot.send_message(message.chat.id, you_out)
+    state[message.from_user.id] = None
+
+
 
